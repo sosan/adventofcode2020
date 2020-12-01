64b8210346d7345c89411943d1f991b1546ca878
jose luis <josloe@gmail.com>
2020-12-01 23:59:53 +0100

    first commit

---
+++ day2.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class DayTwoClass
+{
+    // public static void DayTwo(string[] args)
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("hola");
+
+    }
+}
